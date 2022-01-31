0838a0fbee9f4283f5b15f7f62fe04e38f72751e
MeTonaTOR <metonator@gmail.com>
2022-01-31 20:46:43 +0100

    what a fail.

--- src/main/java/com/soapboxrace/core/api/OpenfireHook.java
+++ src/main/java/com/soapboxrace/core/api/OpenfireHook.java
@@ -84,7 +84,7 @@ public class OpenfireHook {
 
                     if(parameterBO.getBoolParam("SBRWR_NOPU_ENABLE_VOTEMESSAGES")) {
                         for (LobbyEntrantEntity lobbyEntrant : lobbyEntrants) {
-                            openFireSoapBoxCli.send(XmppChat.createSystemMessage("SBRWR_NOPU_USERVOTED," + lobbyEntrant.getPersona().getName() + "," + totalVotes + "," + totalUsersInLobby), personaEntity.getPersonaId());
+                            openFireSoapBoxCli.send(XmppChat.createSystemMessage("SBRWR_NOPU_USERVOTED," + lobbyEntrant.getPersona().getName() + "," + totalVotes + "," + totalUsersInLobby), lobbyEntrant.getPersona().getPersonaId());
                         }
                     }
                 } else if(getActiveLobbyId != 0L && getEventSessionId != 0) {
