06dab1ab084019304b637cf6de9dee8503c5005f
AlternateReality <metonator@gmail.com>
2022-05-04 19:33:16 +0200

    ༼つಠ益ಠ༽つ ─=≡ΣO))

--- src/main/java/com/soapboxrace/core/api/NewsArticles.java
+++ src/main/java/com/soapboxrace/core/api/NewsArticles.java
@@ -30,22 +30,7 @@ public class NewsArticles {
     @GET
     @Secured
     @Produces(MediaType.APPLICATION_XML)
-    /*public ArrayOfNewsArticleTrans newsArticles() {
+    public ArrayOfNewsArticleTrans newsArticles() {
         return newsArticleBO.getNewsArticles(requestSessionInfo.getActivePersonaId());
-    }*/
-    public String newsArticles() {
-        return "<NewsArticleTrans>" +
-            "<ExpiryTime i:nil=\"true\"/>" +
-            "<Filters>2</Filters>" +
-            "<IconType>3</IconType>" +
-            "<LongText_HALId>TXT_NEWS_RARE_ITEM_WON_STREAK</LongText_HALId>" +
-            "<NewsId>234971451</NewsId>" +
-            "<Parameters>11871723|DRIVERX682|23,-1151617747,381|0</Parameters>" +
-            "<PersonaId>11871723</PersonaId>" +
-            "<ShortText_HALId>TXT_NEWS_RARE_ITEM_WON_STREAK_SHORT</ShortText_HALId>" +
-            "<Sticky>0</Sticky>" +
-            "<Timestamp>635704817105530000</Timestamp>" +
-            "<Type>20</Type>" +
-        "</NewsArticleTrans>";
     }
 }
