718f21dff6b85b64eb98606bd4a0038e6a5152f4
AlternateReality <metonator@gmail.com>
2022-10-02 19:12:46 +0200

    minor fix for livery command

--- src/main/java/com/soapboxrace/core/bo/commands/LiveryCommand.java
+++ src/main/java/com/soapboxrace/core/bo/commands/LiveryCommand.java
@@ -12,7 +12,6 @@ import com.soapboxrace.core.jpa.LiveryStoreEntity;
 import com.soapboxrace.core.jpa.PersonaEntity;
 import com.soapboxrace.core.jpa.VinylEntity;
 import com.soapboxrace.core.bo.util.HelpingTools;
-import com.soapboxrace.core.bo.util.OwnedCarConverter;
 import com.soapboxrace.core.bo.*;
 import com.soapboxrace.core.dao.*;
 import com.soapboxrace.core.xmpp.OpenFireSoapBoxCli;
@@ -48,7 +47,7 @@ public class LiveryCommand {
         } else {
             if(command[1].trim().equals("import")) {                
                 if(command.length >= 3) {
-                    LiveryStoreEntity liveryStoreEntity = liveryStoreDao.findLiveryByCode(command[3]);
+                    LiveryStoreEntity liveryStoreEntity = liveryStoreDao.findLiveryByCode(command[2]);
                     if(liveryStoreEntity == null) {
                         openFireSoapBoxCli.send(XmppChat.createSystemMessage("That livery doesn't exists."), personaEntity.getPersonaId());
                     } else {
@@ -102,53 +101,58 @@ public class LiveryCommand {
             } else if(command[1].trim().equals("export")) {
                 //generate the code first
                 CarEntity carEntity = personaBO.getDefaultCarEntity(personaEntity.getPersonaId());
-                Set<VinylEntity> vinyls = carEntity.getVinyls();
 
-                if(vinyls.size() == 0) {
-                    openFireSoapBoxCli.send(XmppChat.createSystemMessage("You can't export livery, this car doesnt have any livery preinstalled on it."), personaEntity.getPersonaId());
-                } else {
-                    String code = HelpingTools.generateCode(parameterBO.getIntParam("SBRWR_LIVERYCODE_LENGTH", 8));
+                if(carEntity != null) {
+                    Set<VinylEntity> vinyls = carEntity.getVinyls();
 
-                    LiveryStoreEntity liveryStoreEntity = new LiveryStoreEntity();
-                    liveryStoreEntity.setPersonaId(personaEntity.getPersonaId());
-                    liveryStoreEntity.setCode(code);
-                    liveryStoreEntity.setCreated(LocalDateTime.now());
-                    liveryStoreEntity.setCarname(carEntity.getName());
-                    liveryStoreDao.insert(liveryStoreEntity);
+                    if(vinyls.size() == 0) {
+                        openFireSoapBoxCli.send(XmppChat.createSystemMessage("You can't export livery, this car doesnt have any livery preinstalled on it."), personaEntity.getPersonaId());
+                    } else {
+                        String code = HelpingTools.generateCode(parameterBO.getIntParam("SBRWR_LIVERYCODE_LENGTH", 8));
 
-                    Integer counter = 1;
-    
-                    //now lets save car vinyls, but first, fetch them!
-                    for (VinylEntity vinyl : vinyls) {
-                        LiveryStoreDataEntity DataEntity = new LiveryStoreDataEntity();
-                        DataEntity.setOrderid(counter);
-                        DataEntity.setHash(vinyl.getHash());
-                        DataEntity.setHue1(vinyl.getHue1());
-                        DataEntity.setHue2(vinyl.getHue2());
-                        DataEntity.setHue3(vinyl.getHue3());
-                        DataEntity.setHue4(vinyl.getHue4());
-                        DataEntity.setLayer(vinyl.getLayer());
-                        DataEntity.setMir(vinyl.isMir());
-                        DataEntity.setRot(vinyl.getRot());
-                        DataEntity.setSat1(vinyl.getSat1());
-                        DataEntity.setSat2(vinyl.getSat2());
-                        DataEntity.setSat3(vinyl.getSat3());
-                        DataEntity.setSat4(vinyl.getSat4());
-                        DataEntity.setScalex(vinyl.getScalex());
-                        DataEntity.setScaley(vinyl.getScaley());
-                        DataEntity.setShear(vinyl.getShear());
-                        DataEntity.setTranx(vinyl.getTranx());
-                        DataEntity.setTrany(vinyl.getTrany());
-                        DataEntity.setVar1(vinyl.getVar1());
-                        DataEntity.setVar2(vinyl.getVar2());
-                        DataEntity.setVar3(vinyl.getVar3());
-                        DataEntity.setVar4(vinyl.getVar4());
-                        DataEntity.setLiverycode(code);
-                        liveryStoreDataDao.insert(DataEntity);
-                        counter++;
+                        LiveryStoreEntity liveryStoreEntity = new LiveryStoreEntity();
+                        liveryStoreEntity.setPersonaId(personaEntity.getPersonaId());
+                        liveryStoreEntity.setCode(code);
+                        liveryStoreEntity.setCreated(LocalDateTime.now());
+                        liveryStoreEntity.setCarname(carEntity.getName());
+                        liveryStoreDao.insert(liveryStoreEntity);
+
+                        Integer counter = 1;
+        
+                        //now lets save car vinyls, but first, fetch them!
+                        for (VinylEntity vinyl : vinyls) {
+                            LiveryStoreDataEntity DataEntity = new LiveryStoreDataEntity();
+                            DataEntity.setOrderid(counter);
+                            DataEntity.setHash(vinyl.getHash());
+                            DataEntity.setHue1(vinyl.getHue1());
+                            DataEntity.setHue2(vinyl.getHue2());
+                            DataEntity.setHue3(vinyl.getHue3());
+                            DataEntity.setHue4(vinyl.getHue4());
+                            DataEntity.setLayer(vinyl.getLayer());
+                            DataEntity.setMir(vinyl.isMir());
+                            DataEntity.setRot(vinyl.getRot());
+                            DataEntity.setSat1(vinyl.getSat1());
+                            DataEntity.setSat2(vinyl.getSat2());
+                            DataEntity.setSat3(vinyl.getSat3());
+                            DataEntity.setSat4(vinyl.getSat4());
+                            DataEntity.setScalex(vinyl.getScalex());
+                            DataEntity.setScaley(vinyl.getScaley());
+                            DataEntity.setShear(vinyl.getShear());
+                            DataEntity.setTranx(vinyl.getTranx());
+                            DataEntity.setTrany(vinyl.getTrany());
+                            DataEntity.setVar1(vinyl.getVar1());
+                            DataEntity.setVar2(vinyl.getVar2());
+                            DataEntity.setVar3(vinyl.getVar3());
+                            DataEntity.setVar4(vinyl.getVar4());
+                            DataEntity.setLiverycode(code);
+                            liveryStoreDataDao.insert(DataEntity);
+                            counter++;
+                        }
+
+                        openFireSoapBoxCli.send(XmppChat.createSystemMessage("Your livery is exported, your code is: " + code), personaEntity.getPersonaId());
                     }
-
-                    openFireSoapBoxCli.send(XmppChat.createSystemMessage("Your livery is exported, your code is: " + code), personaEntity.getPersonaId());
+                } else {
+                    openFireSoapBoxCli.send(XmppChat.createSystemMessage("You can't export livery, this car doesnt have any livery preinstalled on it."), personaEntity.getPersonaId());
                 }
             } else {
                 openFireSoapBoxCli.send(XmppChat.createSystemMessage("Invalid option, try using import or export"), personaEntity.getPersonaId());
