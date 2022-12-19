5ee72c1d0cc4ef3c602ed5ae545a77e737878913
AlternateReality <metonator@gmail.com>
2022-12-19 15:45:09 +0100

    xx

--- src/main/java/com/soapboxrace/core/bo/commands/Debug.java
+++
@@ -1,9 +0,0 @@
-package com.soapboxrace.core.bo.commands;
-
-import javax.ws.rs.core.Response;
-
-public class Debug {
-    public Response Commands() {
-        return Response.noContent().build();
-	}
-}
